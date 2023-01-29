6c390355dbe77da18b6174fc386994d06045e952
krushovice <kickstar69@yandex,ru>
2023-01-29 17:32:26 +1000

    add cli.py

--- brain_games/scripts/cli.py
+++ brain_games/scripts/cli.py
@@ -1,8 +1,5 @@
 import prompt
 
-
-name = prompt.string('May I have your name? ')
-
-
-def welcome_user(name):
-    print(f'Welcome to the Brain Games\nHello, {name} !')
+name = prompt.string('Hello! May I have your name? ')
+def welcome_user():
+    print(f"Welcome to the Brain Games\nLet's play, {name} !")
