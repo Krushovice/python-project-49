de7daf965d233b4b6949ab8f5142fcb2d7845e02
krushovice <kickstar69@yandex,ru>
2023-01-31 17:59:22 +1000

    add brain_calc.py

--- brain_games/scripts/brain_calc.py
+++ brain_games/scripts/brain_calc.py
@@ -1,22 +1,39 @@
 from random import randint, choice
+from cli import name
 import prompt
 
 
 operators = ['+', '-', '*']
-def calculate():
+def calculate(name):
     counter = 0
 
     while counter < 3:
-        num1, num2 = randint(1, 250), randint(1, 250)
+        num1, num2 = randint(1, 100), randint(1, 100)
         operator = choice(operators)
 
-        if num1 > num2:
-            result = f'{num1}{operator}{num2}'
-        else:
-            result = f'{num2}{operator}{num1}'
+        if num1 < num2:
+            if operator == '-':
+                result = num2 - num1
+            elif operator == '+':
+                result = num1 + num2
+            else:
+                result = num1 * num2
 
-        print(f'What is the result of the expression?\nQestion: {result}')
-        correct_answer = result
+        else:
+            if operator == '-':
+                result = num1 - num2
+            elif operator == '+':
+                result = num1 + num2
+            else:
+                result = num1 * num2
+
+        if num1 > num2:
+            quest = f'{num1}{operator}{num2}'
+        else:
+            quest = f'{num2}{operator}{num1}'
+
+        print(f'What is the result of the expression?\nQestion: {quest}')
+        correct_answer = str(result)
         answer = prompt.string('Your answer: ')
         if answer == correct_answer:
             print(f'Current!')
@@ -25,5 +42,8 @@ def calculate():
             print(f'{answer} is wrong answer ;(. Correct answer was {correct_answer}.\nLet"s try again, Sam!')
             counter = 0
 
+    print(f'Congratulations, {name}! You answered correctly 3 times, well done!')
 
-calculate()
+
+if __name__ == '__main__':
+    calculate(name)
