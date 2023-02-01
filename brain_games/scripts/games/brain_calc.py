3ca90c7e835ed5e56862591effd84489866edb3b
krushovice <kickstar69@yandex,ru>
2023-02-01 16:24:08 +1000

    add games/

---
+++ brain_games/scripts/games/brain_calc.py
@@ -0,0 +1,55 @@
+import prompt
+from random import randint, choice
+from brain_games.scripts.cli import welcome_user, name
+
+
+operators = ['+', '-', '*']
+def calculate():
+    welcome_user()
+    counter = 0
+
+    while counter < 3:
+        num1, num2 = randint(1, 100), randint(1, 100)
+        operator = choice(operators)
+
+        if num1 < num2:
+            if operator == '-':
+                result = num2 - num1
+            elif operator == '+':
+                result = num1 + num2
+            else:
+                result = num1 * num2
+
+            quest = f'{num2} {operator} {num1}'
+
+        if num1 > num2:
+            if operator == '-':
+                result = num1 - num2
+            elif operator == '+':
+                result = num1 + num2
+            else:
+                result = num1 * num2
+
+            quest = f'{num1} {operator} {num2}'
+
+
+        print(f'What is the result of the expression?\nQestion: {quest}')
+        correct_answer = str(result)
+        answer = prompt.string('Your answer: ')
+        if answer == correct_answer:
+            print(f'Current!')
+            counter += 1
+        else:
+            print(f'{answer} is wrong answer ;(. Correct answer was {correct_answer}.\nLet"s try again, Sam!')
+            counter = 0
+
+    print(f'Congratulations, {name}! You answered correctly 3 times, well done!')
+
+
+
+def main():
+    calculate()
+
+
+if __name__ == '__main__':
+    main()
---
+++ brain_games/scripts/games/brain_even.py
@@ -0,0 +1,34 @@
+import prompt
+from random import randint
+from brain_games.scripts.cli import welcome_user, name
+
+def parity_check():
+    welcome_user()
+
+    counter = 0
+    while counter < 3:
+        number = randint(1, 100)
+        print('Answer "yes" if the number is even, otherwise answer "no".')
+        print(f'Question: {number}')
+        answer = prompt.string('Your answer: ')
+
+        if (number % 2 == 0 and answer == 'yes') or (number % 2 != 0 and answer == 'no'):
+            print('Current!')
+            counter += 1
+
+        elif number % 2 == 0 and answer == 'no':
+            print(f'Wrong answer! Correct answer is "yes".\nBetter luck next time\nLet"s try again, {name}!')
+
+        elif number % 2 != 0 and answer == 'yes':
+            print(f'Wrong answer! Correct answer is "no".\nBetter luck next time\nLet"s try again, {name}!')
+
+    print(f'Congratulations, {name}!')
+
+
+
+def main():
+    parity_check()
+
+
+if __name__ == '__main__':
+    main()
