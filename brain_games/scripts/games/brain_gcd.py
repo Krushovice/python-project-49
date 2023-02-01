3d036037bce59020bc5a0b6809d6321f95f72e1c
krushovice <kickstar69@yandex,ru>
2023-02-01 18:53:45 +1000

    brain_games/scripts/games/brain_gcd.py

--- brain_games/scripts/games/brain_gcd.py
+++ brain_games/scripts/games/brain_gcd.py
@@ -4,9 +4,8 @@ from brain_games.scripts.cli import welcome_user, name
 
 def find_divisor():
     counter = 0
-
+    print('Find the greatest common divisor of given numbers.')
     while counter < 3:
-        print('Find the greatest common divisor of given numbers.')
         num1, num2 = randint(1, 100), randint(1, 100)
         first_divisors = []
         second_divisors = []
@@ -23,11 +22,11 @@ def find_divisor():
             x += 1
 
         result = [x for x in first_divisors if x in second_divisors]
-        correct_answer = max(result)
+        correct_answer = str(max(result))
 
         question = f'{num1} {num2}'
         print(f'Question: {question}')
-        user_answer = prompt.integer('Your answer: ')
+        user_answer = prompt.string('Your answer: ')
 
         if correct_answer == user_answer:
             print('Coorect!')
@@ -37,7 +36,7 @@ def find_divisor():
             counter = 0
 
 
-        print(f'Congratulations, {name}! You answered correctly 3 times, well done!')
+    print(f'Congratulations, {name}! You answered correctly 3 times, well done!')
 
 
 def main():
