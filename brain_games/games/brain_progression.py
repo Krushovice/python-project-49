5956f47cdd78e74b187381c2a08d7795e024398d
krushovice <kickstar69@yandex,ru>
2023-02-12 13:50:51 +1000

    add  brain_games/games/brain_progression.py

--- brain_games/games/brain_progression.py
+++ brain_games/games/brain_progression.py
@@ -1,40 +1,22 @@
 import prompt
 from random import randint, choice
-from brain_games.scripts.cli import welcome_user, name
+
 
 def find_number():
+    sequence = []
+    number, step = randint(0, 50), randint(0, 50)
+    for i in range(10):
+        number+=step
+        sequence.append(number)
+    return sequence
+
+def build_game():
     print('What number is missing in the progression?')
-    counter = 0
+    sequence = find_number()
+    random_index = randint(0,9)
+    correct_answer = str(sequence[random_index])
+    sequence[random_index] = '..'
+    print('Question: ', end='')
+    print(*sequence)
 
-    while counter < 3:
-        sequence = []
-        number, step = randint(0, 50), randint(0, 50)
-        for i in range(10):
-            number+=step
-            sequence.append(number)
-
-        random_index = randint(0,9)
-        correct_answer = str(sequence[random_index])
-        sequence[random_index] = '..'
-
-        print('Question: ', end='')
-        print(*sequence)
-        user_answer = prompt.string('Your answer:  ')
-
-        if user_answer == correct_answer:
-            print('Correct!')
-            counter+=1
-        else:
-            print(f'{user_answer} is wrong answer ;(. Correct answer was {correct_answer}\nLet"s try again, {name}!')
-            counter = 0
-
-    print(f'Congratulations, {name}! You answered correctly 3 times, well done!')
-
-
-def main():
-    welcome_user()
-    find_number()
-
-
-if __name__ == '__main__':
-    main()
+    return correct_answer
