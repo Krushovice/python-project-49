097f0aac1b743cffe4383e1be034d8dfc0ebd66c
krushovice <kickstar69@yandex,ru>
2023-02-12 14:59:25 +1000

    add .

---
+++ brain_games/scripts/main.py
@@ -0,0 +1,11 @@
+#!/usr/bin/env python3.
+from brain_games.games.engine import start_game
+from brain_games.games import brain_calc, brain_even, brain_gcd,brain_prime, brain_progression
+
+
+def main():
+    start_game(brain_progression)
+
+
+if __name__ == '__main__':
+    main()
