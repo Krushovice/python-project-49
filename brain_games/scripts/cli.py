a08f6e28576b8d19ba8f5158600fd2328131eca9
krushovice <kickstar69@yandex,ru>
2023-02-12 14:05:30 +1000

    add  brain_games/scripts/cli.py

--- brain_games/scripts/cli.py
+++ brain_games/scripts/cli.py
@@ -1,8 +1,6 @@
 import prompt
-name = ''
 def welcome_user():
     print('Welcome to the Brain Games!')
     global name
     name = prompt.string('May I have your name? ')
     print(f'Hello, {name}!')
-    return name
