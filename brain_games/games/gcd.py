7a400240b0461a894da916b118ff17a74f2cc1fb
krushovice <kickstar69@yandex,ru>
2023-02-14 21:41:21 +1000

    add brain_games/games/gcd.py

--- brain_games/games/gcd.py
+++ brain_games/games/gcd.py
@@ -1,4 +1,5 @@
 from random import randint
+from math import gcd
 
 
 QUESTION = 'Find the greatest common divisor of given numbers.'
@@ -7,23 +8,7 @@ QUESTION = 'Find the greatest common divisor of given numbers.'
 def build_game():
     # Задаем 2 числа и находим их делители
     num1, num2 = randint(1, 100), randint(1, 100)
-    first_divisors = []
-    second_divisors = []
-    i = 1
-    while i < num1:
-        if num1 % i == 0:
-            first_divisors.append(i)
-        i += 1
-
-    x = 1
-    while x < num2:
-        if num2 % x == 0:
-            second_divisors.append(x)
-        x += 1
-
-    result = [x for x in first_divisors if x in second_divisors]
-    correct_answer = str(max(result))
-
+    correct_answer = str(gcd(num1, num2))
     task = f'{num1} {num2}'
 
     return correct_answer, task
