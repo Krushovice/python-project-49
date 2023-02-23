6bab7abfb23b8a4c39e41c87ee244fe3dfa90ee0
krushovice <kickstar69@yandex.ru>
2023-02-23 15:13:25 +1000

    fix games

--- brain_games/games/prime.py
+++ brain_games/games/prime.py
@@ -7,13 +7,14 @@ DESCRIPTION = 'Answer "yes" if given number is prime. Otherwise answer "no".'
 
 def make_question():
     task = randint(2, 99)
+    correct_answer = is_prime(task)
+    return correct_answer, task
+
+
+def is_prime(task):
     i = 2
     while i <= sqrt(task):
-
         if task % i == 0:
-            correct_answer = 'no'
-            return
-
+            return 'no'
         i += 1
-    correct_answer = 'yes'
-    return task, correct_answer
+    return 'yes'
